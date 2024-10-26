7a7e4d3fe0f1b9b0db3ac9327c55b9c0818b9219
najbrian <172526679+najbrian@users.noreply.github.com>
2024-10-25 20:10:18 -0700

    added update and delete comment routes and tested. Also added timestamp to comments, and added likes for postSchema and commentSchema

--- controllers/posts.js
+++ controllers/posts.js
@@ -89,4 +89,32 @@ router.post('/:postId/comments', async (req, res) => {
 }
 )
 
+// Update Comment Route
+router.put('/:postId/comments/:commentId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId)
+    const comment = post.comments.id(req.params.commentId)
+    if (!comment.author.equals(req.user._id)) {
+      return res.status(403).json({ error: 'Unauthorized' })
+    }
+    comment.set(req.body)
+    await post.save()
+    res.status(200).json(comment)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+// Delete Comment Route
+router.delete('/:postId/comments/:commentId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId)
+    post.comments.remove({ _id: req.params.commentId })
+    await post.save()
+    res.status(200).json({ message: 'Comment Deleted' })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
 module.exports = router
